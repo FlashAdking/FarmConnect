88ee3aea47720d02efcb7e66479ed72636594c7b
FlashAdking <aditya.tiwade8080@gmail.com>
2026-05-10 16:10:55 +0530

    restructured project

--- WebApplication/src/main/java/com/FarmConnect/WebApplication/repository/TransportRepo.java
+++
@@ -1,9 +0,0 @@
-package com.FarmConnect.WebApplication.repository;
-
-import com.FarmConnect.WebApplication.model.Transporter;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TransportRepo extends MongoRepository<Transporter,Integer> {
-}
